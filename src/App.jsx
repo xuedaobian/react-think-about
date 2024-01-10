1dbdf3f7e0c76582b3fa22a2cfe7089d9524d17a
xuedaobian <1689359591@qq.com>
2024-01-10 22:18:58 +0800

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+
+const App = () => (
+  <div className="App">
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
